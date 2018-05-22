c51b20d07f1b40e0d5619ff84abb3d1e764302bd
Terry Tilley <terry@terrytilley.com>
2018-05-22 21:11:21 +0100

    setup server.js

--- server.js
+++ server.js
@@ -1,1 +1,45 @@
-console.log('Hello world!');
+import 'dotenv/config';
+import qs from 'querystring';
+import express from 'express';
+import request from 'request';
+
+const app = express();
+const port = process.env.PORT || 8888;
+const redirectUri = process.env.REDIRECT_URI;
+
+app.get('/login', (req, res) => {
+  res.redirect(
+    `https://accounts.spotify.com/authorize?${qs.stringify({
+      response_type: 'code',
+      client_id: process.env.SPOTIFY_CLIENT_ID,
+      scope: 'user-read-private user-read-email',
+      redirect_uri: redirectUri,
+    })}`
+  );
+});
+
+app.get('/callback', (req, res) => {
+  const code = req.query.code || null;
+  const authOptions = {
+    url: 'https://accounts.spotify.com/api/token',
+    form: {
+      code,
+      redirect_uri: redirectUri,
+      grant_type: 'authorization_code',
+    },
+    headers: {
+      Authorization: `Basic ${Buffer.from(
+        `${process.env.SPOTIFY_CLIENT_ID}:${process.env.SPOTIFY_CLIENT_SECRET}`
+      ).toString('base64')}`,
+    },
+    json: true,
+  };
+  request.post(authOptions, (error, response, body) => {
+    const uri = process.env.FRONTEND_URI;
+    const accessToken = body.access_token;
+
+    res.redirect(`${uri}?access_token=${accessToken}`);
+  });
+});
+
+app.listen(port, () => console.log(`Listening on port ${port}`));
